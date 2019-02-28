bb59e3c6734e20a5ba23b233b6855ef95dc3e746
Dung Cry <824264439@qq.com>
2019-02-28 17:46:18 +0800

    增加数据库测试相关Demo

--- application/cat/controller/Index.php
+++ application/cat/controller/Index.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\cat\controller;
 
+use think\Db;
+
 /**
  * 购物猫相关类
  */
@@ -16,7 +18,23 @@ class Index
      */
     public function testDb()
     {
-
+        // 数据库查询
+        $result = Db::query('select * from cat_wx_app_status where version_id=:id', ['id' => '1.0.0']);
+        // 数据库增加
+        $insertResult = Db::execute('insert into cat_wx_app_status (version_id, status) values (:id, :status)', ['id' => '1.0.2', 'status' => '1']);
+        // 数据库删除
+        // $delResult = Db::execute('delete from cat_wx_app_status where version_id=:id', ['id' => '1.0.2']);
+        // 数据库变更
+        $updateResult = Db::execute('update  cat_wx_app_status set status=:status where version_id=:id', ['status' => 0, 'id' => '1.0.2']);
+        // 测试数据库事物
+        Db::startTrans();
+        try {
+            $insertResult = Db::execute('insert into cat_wx_app_status (version_id, status) values (:id, :status)', ['id' => '1.0.2', 'status' => '1']);
+            $updateResult = Db::execute('update  cat_wx_app_status set status=:status where version_id=:id', ['status' => 5, 'id' => '1.0.2']);
+        } catch (\Throwable $th) {
+            Db::rollback();
+        }
+        return json(['status' => '1', 'messange' => '操作成功', 'data' => '']);
     }
-    
+
 }
