0dde161492e6e943ec999c38de10bbc7741e5e99
yuanxx <1763.435687@163.com>
2019-03-07 14:59:50 +0800

    添加Querylist依赖

--- application/cat/controller/Api.php
+++ application/cat/controller/Api.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\cat\controller;
 
+use QL\QueryList;
+
 /**
  * 购物猫相关api接口
  */
@@ -10,4 +12,17 @@ class Api
     {
         return 'this is api';
     }
+
+    /**
+     * 获取首页海报
+     * @return:
+     */
+    public function getIndexBanner()
+    {
+        //采集某页面所有的图片
+        $data = QueryList::get('http://cms.querylist.cc/bizhi/453.html')->find('img')->attrs('src');
+        //打印结果
+        print_r($data->all());
+    }
+
 }
