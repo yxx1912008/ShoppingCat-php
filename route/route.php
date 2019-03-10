0b93baf8deb7ecb1f4b85e937f1c74dbaba8ed6b
yxx19912008 <82426439@qq.com>
2019-03-10 23:10:07 +0800

    添加疯抢商品列表路由

--- route/route.php
+++ route/route.php
@@ -16,7 +16,9 @@ Route::post('hello/:name', 'index/hello');
 // 获取首页海报
 Route::post('cat/api/getIndexBanner.do', 'cat/api/getIndexBanner');
 //查询微信小程序状态
-Route::post('cat/api/getWxAppStatus.do','cat/api/getWxAppStatus');
+Route::post('cat/api/getWxAppStatus.do', 'cat/api/getWxAppStatus');
+//获取正在疯抢商品列表商品列表
+Route::post('cat/api/getCurrentQiang.do', 'cat/api/getCurrentQiang');
 
 return [
 
