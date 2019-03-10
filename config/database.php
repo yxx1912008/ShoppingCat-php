dc38d02a6ba440be5ce0ea4605723917280b92d0
yxx19912008 <82426439@qq.com>
2019-03-10 20:22:47 +0800

    修改数据库为本地

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '192.168.1.104',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'ShopCat',
     // 用户名
-    'username'        => 'ShopCat',
+    'username'        => 'root',
     // 密码
-    'password'        => 'EGpKhsaWhmz7i2mr',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
