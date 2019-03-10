ddbe0ddf5245a3abb8f07027d652e3094f14ece8
yxx19912008 <82426439@qq.com>
2019-03-10 23:09:45 +0800

    修改购物猫首页地址

--- config/cat/app.php
+++ config/cat/app.php
@@ -1,7 +1,7 @@
 <?php
 //配置文件
 return [
-    'CAT_URL' => 'http://cat.gouwumao.top/index.php', //购物猫首页地址
+    'CAT_URL' => 'http://cat.gouwumao.top/index.php?', //购物猫首页地址
     'CAT_API_URL' => 'https://lucky.oicp.vip', //购物猫Api接口地址
     'BANNER_CACHE_TIME' => 60 * 60 * 6, //首页海报缓存时间
 ];
