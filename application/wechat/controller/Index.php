a9196150fd42bce8cd00777ef75a5aa2e0a70037
yuanxx <17630435687@163.com>
2019-03-19 18:08:45 +0800

    完善微信公众号接口

--- application/wechat/controller/Index.php
+++ application/wechat/controller/Index.php
@@ -18,8 +18,6 @@ class Index
     public function wxAuthen(Request $request)
     {
         if ($request->method() == 'GET') {
-            Log::error('日志信息:' . $request->get('echostr'));
-
             $echostr = $request->get('echostr');
             $nonce = $request->get('nonce');
             $signature = $request->get('signature');
@@ -31,8 +29,9 @@ class Index
                 return $echostr;
             }
             return 'failed';
-        } else {
-//post
+        } else { //post
+            $this->handleWechat();
+            return 'OK';
         }
     }
 
@@ -41,12 +40,80 @@ class Index
      */
     public function checkSignature($timestamp, $echostr, $nonce, $signature)
     {
-        $token = '8ff953dd97c4405234a04291dee39e0b';
+        $token = '8ff953dd97c4405234a04291dee39e0b'; //微信管理后台设置的token
         $result = SHA1::getSHA1($token, $timestamp, $nonce, '');
         if ($result[0] == 0 && strcasecmp($signature, $result[1]) == 0) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 处理微信请求
+     */
+    public function handleWechat()
+    {
+        // $postArr = $GLOBALS['HTTP_RAW_POST_DATA']; //读取微信传过来的文件流
+        $postArr = file_get_contents("php://input");
+        Log::error($postArr);
+        $postObj = simplexml_load_string($postArr);
+
+        if (strtolower($postObj->MsgType) == 'text') { //是否是用户的文本消息
+            $this->handleText($postObj);
+        }
+
+        return 'OK';
+    }
+
+    /**
+     * 处理用户发来的微信消息
+     */
+    private function handleText($postObj)
+    {
+        //回复用户消息(纯文本格式)
+        $toUser = $postObj->FromUserName;
+        $fromUser = $postObj->ToUserName;
+        $recText = (String) $postObj->Content;
+        $content = '';
+        Log::error($recText);
+        if (strpos($recText, '优惠券') !== false) { //查询字符串中是否包含
+            $content = str_replace('优惠券', '', $recText);
+        } else {
+            $content = '没找到';
+        }
+        $this->returText($fromUser, $toUser, $content);
+    }
+
+    /**
+     * 返回纯文本微信消息
+     */
+    private function returText($fromUser, $toUser, $content)
+    {
+        $time = time();
+        $msgType = 'text';
+        $template = "<xml>
+        <ToUserName><![CDATA[%s]]></ToUserName>
+        <FromUserName><![CDATA[%s]]></FromUserName>
+        <CreateTime>%s</CreateTime>
+        <MsgType><![CDATA[%s]]></MsgType>
+        <Content><![CDATA[%s]]></Content>
+        </xml>";
+        $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+        echo $info;
+    }
+
+    /**
+     * 搜索优惠券商品
+     */
+    public function searchGood($keyWord)
+    {
+        $catUrl = Config('CAT_URL') . 'r=index%2Fsearch&s_type=1&kw=' . $keyWords;
+        $res = requestUrl($catUrl, 'GET');
+        $pattern = '/dtk_data=(.*?);/'; //正则匹配规则
+        if (!empty($res) && preg_match($pattern, $res, $result)) {
+            return $result[1];
+        }
+        return null;
     }
 
 }
--- config/wechat/app.php
+++ config/wechat/app.php
@@ -1,5 +1,5 @@
 <?php
 //配置文件
 return [
-
+    'CAT_URL' => 'http://cat.gouwumao.top/index.php?', //购物猫首页地址
 ];
