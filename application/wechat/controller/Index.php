ea578955fbaf5291930969aca7c3c6c329836a82
yuanxx <17630435687@163.com>
2019-03-20 10:15:49 +0800

    完善微信公众号搜索商品优惠券

--- application/wechat/controller/Index.php
+++ application/wechat/controller/Index.php
@@ -58,6 +58,12 @@ class Index
         Log::error($postArr);
         $postObj = simplexml_load_string($postArr);
 
+        if (strtolower($postObj->MsgType) == 'event') {
+            if (strtolower($postObj->Event == 'subscribe')) { //如果是订阅事件
+                $this->handleSubscribe($postObj->ToUserName, $postObj->FromUserName);
+            }
+        }
+
         if (strtolower($postObj->MsgType) == 'text') { //是否是用户的文本消息
             $this->handleText($postObj);
         }
@@ -75,9 +81,15 @@ class Index
         $fromUser = $postObj->ToUserName;
         $recText = (String) $postObj->Content;
         $content = '';
-        Log::error($recText);
         if (strpos($recText, '优惠券') !== false) { //查询字符串中是否包含
             $content = str_replace('优惠券', '', $recText);
+            $goodInfos = $this->searchGood($content); //搜索到的商品
+            $result = [
+                'title' => $goodInfos[0]['d_title'],
+                'description' => $goodInfos[0]['description'],
+                'picUrl' => $goodInfos[0]['pic'],
+            ];
+            return;
         } else {
             $content = '没找到';
         }
@@ -103,17 +115,54 @@ class Index
     }
 
     /**
+     * 处理订阅事件
+     */
+    public function handleSubscribe($fromUser, $toUser)
+    {
+        $content = '/:heart欢迎关注购物猫 /:rose\r\n/:heart查找优惠券，请在前面加 “优惠券“\r\n/:heart例如：想找耳机优惠券，输入： 优惠券 耳机 \r\n/:heart指定商品查找优惠券，先复制淘宝商品完整标题 \r\n/:heart输入 :优惠券 淘宝标题，然后发给我\r\n/:coffee查找电影，请在电影名前加“电影”\r\n/:coffee要求管理员添加电影 请在电影名前加“添加”';
+        $this->returText($fromUser, $toUser, $content);
+    }
+
+    /**
      * 搜索优惠券商品
      */
     public function searchGood($keyWord)
     {
-        $catUrl = Config('CAT_URL') . 'r=index%2Fsearch&s_type=1&kw=' . $keyWords;
+        $catUrl = Config('CAT_URL') . 'r=index%2Fsearch&s_type=1&kw=' . $keyWord;
+        Log::error('请求网址：'.$catUrl);
         $res = requestUrl($catUrl, 'GET');
+        Log::error($res);
         $pattern = '/dtk_data=(.*?);/'; //正则匹配规则
         if (!empty($res) && preg_match($pattern, $res, $result)) {
-            return $result[1];
+            return json_decode($result[1], true); //转换为数组
         }
-        return null;
+        return;
+    }
+
+/**
+ * 回复图文消息
+ */
+    public function returnNews($fromUser, $toUser, $content)
+    {
+        $time = time();
+        $msgType = 'news';
+        $template = '<xml>
+        <ToUserName><![CDATA[%s]]></ToUserName>
+        <FromUserName><![CDATA[%s]]></FromUserName>
+        <CreateTime>%s</CreateTime>
+        <MsgType><![CDATA[news]]></MsgType>
+        <ArticleCount>1</ArticleCount>
+        <Articles>
+          <item>
+            <Title><![CDATA[%s]]></Title>
+            <Description><![CDATA[%s]]></Description>
+            <PicUrl><![CDATA[%s]]></PicUrl>
+            <Url><![CDATA[%s]]></Url>
+          </item>
+        </Articles>
+      </xml>';
+        $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content['title'], $content['description'], $content['picUrl'], '');
+        echo $info;
     }
 
 }
